d065ecbfc262d085bfcca1c6dad63ed218036d88
hortensia2099 <b2220790@photon.chitose.ac.jp>
2023-11-14 23:36:52 +0900

    Selectクラスを作成した

---
+++ db_prac/db_prac/src/main/java/jp/ac/chitose/db_prac/PreExam.java
@@ -0,0 +1,21 @@
+package db_prac.src.main.java.jp.ac.chitose.db_prac;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+public class PreExam {
+    private String gakusekiCode;
+    private String familyName;
+    private String firstName;
+    private int point;
+
+    public PreExam(String gakusekiCode,String familyName,String firstName,int point){
+        this.gakusekiCode = gakusekiCode;
+        this.familyName = familyName;
+        this.firstName = firstName;
+        this.point = point;
+    }
+
+    public void print(){
+        System.out.println(gakusekiCode+"さんぽ"+point+"点");
+    }
+}
